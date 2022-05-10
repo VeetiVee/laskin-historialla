f21abcfde2ec6274a8017c236552355df19a6682
VeetiVee <veeti.vainikka@gmail.com>
2022-05-10 16:00:39 +0300

    forgot to push

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from "react-native";
 
 export default function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [total, setTotal] = useState("");
+  const [data, setData] = useState([]);
+
+  const plus = () => {
+    const calc = number1 + number2;
+    setTotal(calc);
+    setNumber1("");
+    setNumber2("");
+    const entry = number1 + "+" + number2 + "=" + calc;
+    setData([...data, { key: entry }]);
+  };
+
+  const minus = () => {
+    const calc = number1 - number2;
+    setTotal(calc);
+    setNumber1("");
+    setNumber2("");
+    const entry = number1 + "-" + number2 + "=" + calc;
+    setData([...data, { key: entry }]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {total} </Text>
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        keyboardType={"number-pad"}
+        onChangeText={(text) => setNumber1(parseInt(text))}
+        value={number1}
+      />
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        keyboardType={"number-pad"}
+        onChangeText={(text) => setNumber2(parseInt(text))}
+        value={number2}
+      />
+      <View style={{ flexDirection: "row" }}>
+        <Button onPress={plus} title="+" />
+        <Button onPress={minus} title="-" />
+      </View>
+      <h7>History</h7>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +60,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
